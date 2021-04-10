79f32211daf992104a8a845bf145ab7ec356ec9c
mgibowski <gibowski@gmail.com>
2021-04-10 10:43:44 +0100

    Initial commit

---
+++ lib/elixir_gitpod.ex
@@ -0,0 +1,18 @@
+defmodule ElixirGitpod do
+  @moduledoc """
+  Documentation for `ElixirGitpod`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirGitpod.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_gitpod_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirGitpodTest do
+  use ExUnit.Case
+  doctest ElixirGitpod
+
+  test "greets the world" do
+    assert ElixirGitpod.hello() == :world
+  end
+end
